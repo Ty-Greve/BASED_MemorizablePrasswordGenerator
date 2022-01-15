bb3fbd690dcda7e9d7a865afa01ba24b1d24cf07
cinnamonhater <89957213+cinnamonhater@users.noreply.github.com>
2022-01-15 16:10:06 -0700

    Function LetterToNumber updated

--- Randomizer.py
+++ Randomizer.py
@@ -6,3 +6,28 @@ class Randomizer:
   def __init__():
     pass
   def Anagram()
+  
+  def LetterToNumber(letter):
+    
+    letter.lower()
+    
+    if letter == 'i':
+      return '1';
+    elif letter == 'z':
+      return '2'
+    elif letter == 'e':
+      return '3'
+    elif letter == 'a':
+      return '4'
+    elif letter == 's':
+      return 5
+    elif letter == 'g':
+      return 6
+    elif letter == 't':
+      return 7
+    elif letter == 'b':
+      return '8'
+    elif letter == 'g':
+      return '9'
+    elif letter == 'o':
+      return '0'
