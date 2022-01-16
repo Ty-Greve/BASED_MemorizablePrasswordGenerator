1fc4201d25bd7395e5ad1612c0a21ba268ed06a2
jfernan123 <91108435+jfernan123@users.noreply.github.com>
2022-01-16 09:54:06 -0700

    Update createPassword.py

--- Code/createPassword.py
+++ Code/createPassword.py
@@ -1,5 +1,3 @@
-from Randomizer.py import Randomizer
-
 def createPassword(userAnswers, passwordLength):
   randomObj = Randomizer()
   for object in userAnswers:
@@ -8,8 +6,6 @@ def createPassword(userAnswers, passwordLength):
 
   ORIGINALANSWERS = userAnswers.copy()
   #We will mix in the password through multiple layers 
-  randomTreatment = [userAnswers[0], userAnswers[1], userAnswers[3]]
-  random.shuffle(randomTreatment)
   
   #--This will never change one word twice, as range random.randrange only selects a number between 0 and 3, but in this case just 
   #selecting the number of items to be changed randomly, so regardless, each item will keep is original placement in the list, and therefore
@@ -17,17 +13,21 @@ def createPassword(userAnswers, passwordLength):
   
   
   #Pick a random number of words to be selected from 1 to 3
-  for i in range(random.randrange(3))
+  for i in range(random.randrange(3)):
     diceRoll = random.randint(1,4)
     #Try to apply the anagramFinder and the similarSound
     if (diceRoll == 1):
-      userTreatment[i] = randomObj.anagramFinder(userTreatment[i])
+      print(1)
+      userAnswers[i] = randomObj.findAnagram(userAnswers[i])
     elif (diceRoll == 2):
-      userTreatment[i] = randomObj.similarSound(userTreatment[i])
+      print(2)  
+      userAnswers[i] = randomObj.similarSound(userAnswers[i])
     elif (diceRoll ==3):
-      userTreatment[i] = randomObj.LetterToNumber(userTreatment[i])
+      print(3)
+      userAnswers[i] = randomObj.LetterToNumber(userAnswers[i])
     else:
-      userTreatment[i] = randomObj.TakeLetterWay(userTreatment[i])
+      print(4)
+      userAnswers[i] = randomObj.TakeLetterAway(userAnswers[i])
       
       
        
@@ -36,17 +36,17 @@ def createPassword(userAnswers, passwordLength):
   
   
     
-  
+  print(userAnswers)
   megaStrPassword = userAnswers[0] + userAnswers[1] + userAnswers[2]    #concatenating all the string responses
   length = len(megaStrPassword)                                         
-  index1 = random.randint(length)
-  index2 = random.randint(length)                                       #Finding 2 random integers to be the range of the slice
+  index1 = random.randrange(length)
+  index2 = random.randrange(length)                                       #Finding 2 random integers to be the range of the slice
   
   if index1<index2:                                                     #Extracting a slice from the mega string
     strPwdSlice = megaStrPassword[index1:index2]
     upper = index1
     lower = index2
-  else
+  else:
     upper = index2
     lower = index1
     strPwdSlice = megaStrPassword[index2:index1]
@@ -57,12 +57,12 @@ def createPassword(userAnswers, passwordLength):
 #Check the upper and lower boundaries with the mega string, and selects the appropiate original word 
   if lower <  len(userAnswers[0]): #If lower bundary is less than the length of the original word, then that means that the first word was in
     wordMix.append(ORIGINALANSWERS[0])
-  elif len(usersAnswers[0])< lower < len(userAnswers[0] + len(userAnswers[1]): #If lower bound is between the first and second words, that means the second word was in
+  elif len(userAnswers[0])< lower < len(userAnswers[0]) + len(userAnswers[1]):   #If lower bound is between the first and second words, that means the second word was in
     wordMix.append(ORIGINALANSWERS[1])
   else:
     wordMix.append(ORIGINALANSWERS[2]) #Otherwise this means that the third word was in 
 
-  if len(usersAnswers[0])< upper < len(userAnswers[0]) + len(userAnswers[1]) and userAnswers[1] not in wordMix: #Same logic applies with the upper bound, but in this case
+  if len(userAnswers[0])< upper < len(userAnswers[0]) + len(userAnswers[1]) and userAnswers[1] not in wordMix: #Same logic applies with the upper bound, but in this case
                                                                                                                 #we make sure that this is not already in the wordMix
      wordMix.append(ORIGINALANSWERS[1])
   if upper >= len(userAnswers[0]) + len(userAnswers[1]) and userAnswers[2] not in wordMix:
@@ -70,15 +70,17 @@ def createPassword(userAnswers, passwordLength):
   
   index = random.randint(3,5)                         #Index of the number chosen
   chosennumber = userAnswers[index]                   #Number chosen that is included in the password
+  print(password)
   password.insert(0,chosennumber)
      
   
   usedchars = []
-  charlist = ['~', '@', '#', '$', '%', '^', '&'. '*'. '('. '(', '-', '+', '[', ']', '{', '}', '\', '|', '<', '>', '/'. '?']
+  charlist = ['~', '@', '#', '$', '%', '^', '&', '*', '(', '(', '-', '+', '[', ']', '{', '}', '|', '<', '>', '/', '?']
   if len(password) < passwordLength:
       currlength = len(password)
   while currlength < passwordLength:
       random.shuffle(charlist)
       usedchars.append(charlist[0])                       #List of special characters used
-      password[currlength] = charlist[0]                  #password is the final password
-       
+      password[currlength] = charlist[0]
+
+createPassword(["Potato","Tomato", "Yam","12","15","16"], 12)
